e14bf91db76d87e4b5882570df9340d7441b06b8
Lauren Nolan <laurenn1468@gmail.com>
2018-01-30 16:21:09 +0000

    changed db credentials

--- phpsqlajax_dbinfo.php
+++ phpsqlajax_dbinfo.php
@@ -1,5 +1,11 @@
 <?php
-$username="root";
-$password="";
+$servername="eu-cdbr-west-02.cleardb.net";
+$username="b13ea83013e970";
+$password="d8412a08";
 $database="map";
+
+// $servername="localhost"
+// $username="root";
+// $password="";
+// $database="map";
 ?>
